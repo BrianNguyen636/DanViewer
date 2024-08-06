eece51a29680ec8422e9fe47ed0fe95f97c5d982
Brian Nguyen <briannguyen636@gmail.com>
2024-08-05 17:49:37 -0700

    First commit

---
+++ src/components/searchbar.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+export default function SearchBar() {
+    return (
+        <div>
+            Search:
+            <input type='text'></input>
+        </div>
+    );
+}
