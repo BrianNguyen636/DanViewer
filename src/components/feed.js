d4c83a9fec2f287a5a78b228cdcae5d06f770250
Brian Nguyen <briannguyen636@gmail.com>
2024-08-24 15:09:17 -0700

    Refactored fetch to new function

--- src/components/feed.js
+++ src/components/feed.js
@@ -4,24 +4,30 @@ import {useState, React} from 'react';
 const LOGIN = 'avianbot';
 const KEY = 'TekDM7Yef4WALQmM5sE28b4G'
 
+async function getResponse(params) {
+    let page = 1;
+    let url = 'https://danbooru.donmai.us/posts.json?'+
+    'page=' + page 
+    +'&login='+LOGIN+'&api_key='+KEY;
+    let response = await fetch(url,
+        {method:"GET", mode:"cors"}
+    )
+    return await response.json();
+}
+
 export default function Feed() {
 
     const [response, setResponse] = useState({});
     const [status, setStatus] = useState('');
     const [loading, setLoading] = useState(true);
-    let page = 1;
-    let url = 'https://danbooru.donmai.us/posts.json?'+
-    'page=' + page 
-    +'&login='+LOGIN+'&api_key='+KEY;
 
-        fetch(url,
-            {method:"GET", mode:"cors"}
-        )
-            .then((response)=>response.json())
-            .then((response)=>{
-                setResponse(response)
-                setLoading(false)
-    })
+    getResponse().then(
+        (response) => {
+            setLoading(false);
+            setResponse(response);
+        }
+    )
+
     return (
         <div>
             Posts: {response.length}
